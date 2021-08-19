fd1b41420e9a1f60ab6db0db164837e63a6d85cf
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-08-19 19:22:22 +0100

    push

--- resume_template/settings.py
+++ resume_template/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9c0czp(6)9#os#msg#l9mt=0gi*1vq0_uqmkz$alb7^lr=z+4h'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
