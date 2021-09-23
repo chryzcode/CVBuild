104394497d59662192522a7b361a1a74cf1c51e5
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-09-23 23:15:44 +0100

    commit

--- resume_template/settings.py
+++ resume_template/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = os.getenv(str('SECRET_KEY'))
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
