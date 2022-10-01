228cfecc858da18597566ae1cc4b045496c9b6ee
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-10-01 15:52:11 +0100

    push

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Project)
 admin.site.register(Skill_Level)
 admin.site.register(Language_Level)
--- app/models.py
+++ app/models.py
@@ -14,6 +14,31 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
+class CustomAccountManager(BaseUserManager):
+    def create_superuser(self, email, password, **other_fields):
+
+        other_fields.setdefault("is_staff", True)
+        other_fields.setdefault("is_superuser", True)
+        other_fields.setdefault("is_active", True)
+
+        if other_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must be assigned to is_staff=True.")
+        if other_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must be assigned to is_superuser=True.")
+
+        return self.create_user(email, password, **other_fields)
+
+    def create_user(self, email, password, **other_fields):
+
+        if not email:
+            raise ValueError(_("The email field is required"))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
     first_name = models.CharField(max_length=150)
@@ -24,9 +49,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    objects = CustomAccountManager()
+
     USERNAME_FIELD = "email"
 
-    REQUIRED_FIELDS = ["first_name, last_name"]
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
         return self.email
