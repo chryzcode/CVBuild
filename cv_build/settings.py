db01805161a0f162dda6d203cb4aa6a755146939
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2023-02-24 20:16:21 +0100

    push

--- cv_build/settings.py
+++ cv_build/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config("DEBUG", default=False, cast=bool)
+# DEBUG = config("DEBUG", default=False, cast=bool)
+
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "cv-build.onrender.com"]
 
