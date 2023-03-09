1d915e49a51ef539cc026d55a6845a70374aa6e8
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2023-03-09 22:11:51 +0100

    push

--- cv_build/urls.py
+++ cv_build/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    path('', include('django.contrib.auth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "app.views.custom_error_404"
