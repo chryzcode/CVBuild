ad6bda24cb4cd8d8b2acc20ff6f977bd31cc5755
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2023-10-18 08:13:57 +0100

    push

--- cv_build/settings.py
+++ cv_build/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "cv-build.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "cv-build.onrender.com", "cvbuild-c354d1c4f98a.herokuapp.com"]
 
 
 # Application definition
